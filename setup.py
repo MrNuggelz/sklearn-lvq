7c0dd6fdf60e18f44a3216b25b610645583086aa
jjensen <jjensen@techfak.uni-bielefeld.de>
2017-09-26 16:28:52 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -17,12 +17,32 @@ except ImportError:
     print('scipy is required during installation')
     sys.exit(1)
 
+CLASSIFIERS = [
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Intended Audience :: Science/Research',
+    'Natural Language :: English',
+    'Operating System :: POSIX',
+    'Operating System :: MacOS',
+    'Operating System :: Unix',
+    'Operating System :: Microsoft :: Windows',
+]
+
+version='1.0.2'
+
 setup(name='sklearn-glvq',
-      version='1.0.1',
+      version=version,
       description='sklearn compatible Generalized Learning Vector '
                   'Quantization implementation',
       author='Joris Jensen',
+      url='https://github.com/MrNuggelz/sklearn-glvq',
+      download_url='https://github.com/MrNuggelz/sklearn-glvq/releases/tag/{}'.format(version),
+      tests_require=['nose'],
+      platforms=['any'],
+      license='BSD-3-Clause',
       packages=['glvq'],
       install_requires=INSTALL_REQUIRES,
       author_email='jjensen@techfak.uni-bielefeld.de',
+      classifiers=CLASSIFIERS,
       )
