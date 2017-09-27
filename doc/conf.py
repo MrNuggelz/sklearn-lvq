68e167f8d1db56c0bf0c2c5297deb60f997c9631
jjensen <jjensen@techfak.uni-bielefeld.de>
2017-09-27 18:39:06 +0200

    sphinx_gallery fix

--- doc/conf.py
+++ doc/conf.py
@@ -14,9 +14,10 @@
 
 import sys
 import os
-
 import sphinx_rtd_theme
+import matplotlib
 
+matplotlib.use('Agg')
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
