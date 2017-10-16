86f0f8c551693e44304de9ff692452d2f7e50d50
jjensen <jjensen@techfak.uni-bielefeld.de>
2017-10-16 12:27:13 +0200

    changed plot_glvq

--- examples/plot_glvq.py
+++ examples/plot_glvq.py
@@ -12,6 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from glvq import GlvqModel
+from glvq.plot_2d import to_tango_colors, tango_color
 
 print(__doc__)
 
@@ -26,9 +27,12 @@ glvq = GlvqModel()
 glvq.fit(toy_data, toy_label)
 pred = glvq.predict(toy_data)
 
-plt.scatter(toy_data[:, 0], toy_data[:, 1], c=toy_label)
-plt.scatter(toy_data[:, 0], toy_data[:, 1], c=pred, marker='.')
-plt.scatter(glvq.w_[:, 0], glvq.w_[:, 1])
+plt.scatter(toy_data[:, 0], toy_data[:, 1], c=to_tango_colors(toy_label), alpha=0.5)
+plt.scatter(toy_data[:, 0], toy_data[:, 1], c=to_tango_colors(pred), marker='.')
+plt.scatter(glvq.w_[:, 0], glvq.w_[:, 1],
+            c=tango_color('aluminium', 5), marker='D')
+plt.scatter(glvq.w_[:, 0], glvq.w_[:, 1],
+            c=to_tango_colors(glvq.c_w_, 0), marker='.')
 plt.axis('equal')
 
 print('classification accuracy:', glvq.score(toy_data, toy_label))
