15e77523aef76152ca517bcaab91cc85c280580a
Jasper de Boer <mail@jasper.frl>
2018-04-18 20:57:24 +0200

    Add support for classes starting from != 0 for rslvq decision_function. Remove wrongfull predict_proba implementation for rslvq

--- sklearn_lvq/glvq.py
+++ sklearn_lvq/glvq.py
@@ -262,12 +262,9 @@ class GlvqModel(_LvqBaseModel):
                              "expected=%d" % (self.w_.shape[1], x.shape[1]))
         dist = self._compute_distance(x)
 
-        res = dist[:,self.c_w_ != 0].min(1) - dist[:,self.c_w_ == 0].min(1)
+        foo = lambda c: dist[:,self.c_w_ != self.classes_[c]].min(1) - dist[:,self.c_w_ == self.classes_[c]].min(1)
+        res = np.vectorize(foo, signature='()->(n)')(self.classes_).T
 
-        for c in range(self.classes_.size-1):
-           res = np.vstack((res, dist[:,self.c_w_ != c+1].min(1) - dist[:,self.c_w_ == c+1].min(1)))
-
-        res = res.T
         if self.classes_.size <= 2:
             return res[:,1]
         else:
