e6763783324d905bde3e784eb7a523fa1a615775
RJSietsma <r.j.sietsma@st.hanze.nl>
2019-11-20 15:22:12 +0100

    Ignore this

--- sklearn_lvq/gmlvq.py
+++ sklearn_lvq/gmlvq.py
@@ -19,6 +19,8 @@ from sklearn.utils import validation
 class GmlvqModel(GlvqModel):
     """Generalized Matrix Learning Vector Quantization
 
+    this is a push test, ignore
+
     Parameters
     ----------
 
